2c78ffb7ccbeaa934a7429f69e0dedcfbec9f468
sonam1812 <sonampal832@gmail.com>
2021-08-30 09:53:32 +0530

    Added: UC1-PrintBoard

--- main/java/com/bridgelabz/Tic_Tac_Toe_Game/Welcome.java
+++ main/java/com/bridgelabz/Tic_Tac_Toe_Game/Welcome.java
@@ -1,9 +1,24 @@
 package com.bridgelabz.Tic_Tac_Toe_Game;
 
 public class Welcome {
+	
+   public static final char[] charArray = new char[10];
 	public static void main(String[] args) {
-		System.out.println("Welcome to the game ");
-		
+		printBoard();
 	}
-
+	public static void printBoard() {
+		int count = 0;
+		int i;
+		for (i = 1; i < charArray.length; i++) {
+			charArray[i] = ' ';
+			System.out.println(charArray[i]);
+			
+		}
+	}
 }
+		
+		
+			
+		
+
+
