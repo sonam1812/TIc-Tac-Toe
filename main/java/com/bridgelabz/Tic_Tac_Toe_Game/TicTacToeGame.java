f47be1a6a08ec77a46e1ce0315073b87611663d0
sonam1812 <sonampal832@gmail.com>
2021-08-31 16:25:20 +0530

    Added: Displayboard

--- main/java/com/bridgelabz/Tic_Tac_Toe_Game/TicTacToeGame.java
+++ main/java/com/bridgelabz/Tic_Tac_Toe_Game/TicTacToeGame.java
@@ -3,17 +3,23 @@ package com.bridgelabz.Tic_Tac_Toe_Game;
 import java.util.Scanner;
 
 public class TicTacToeGame {
-	
+	public static char[] box= new char[10];
    public static final char[] charArray = new char[10];
+   
 	public static void main(String[] args) {
 		printBoard();
 		playGame();
+		displayBoard();
+		
 	}
+
+
 	public static void printBoard() {
 		int count = 0;
 		int i;
 		for (i = 1; i < charArray.length; i++) {
 			charArray[i] = ' ';
+			box[i] = ' ';
 			System.out.println(charArray[i]);
 			
 		}
@@ -33,8 +39,25 @@ public class TicTacToeGame {
 			System.out.println("player turn 0");
 			System.out.println("Computer turn X");
 		}
+		else {
+			System.out.println("Invalid!!!!");
+		}
 	}
-}
+		public static void displayBoard() {
+			
+			System.out.println("      |      |      ");
+			System.out.println("   " + box[1] + "  |  " + box[2] + "   |  " + box[3] + "   ");
+            System.out.println("______|______|_____");
+            System.out.println("      |      |      ");
+            System.out.println("   " + box[4] + "  |  " + box[5] + "   |  " + box[6] + "   ");
+            System.out.println("______|______|_____");
+            System.out.println("      |      |      ");
+            System.out.println("   " + box[7] + "  |  " + box[8] + "   |  " + box[9] + "   ");
+            System.out.println("      |      |      ");
+		}
+           
+		}
+
 
 
 		
